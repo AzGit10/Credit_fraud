4e118a069320d2d054b93d2c989fb4866afcb1e4
AzGit10 <164875821+AzGit10@users.noreply.github.com>
2025-04-17 22:56:53 +0100

    Latest Reports.py

--- pages/Reports.py
+++ pages/Reports.py
@@ -1,18 +1,26 @@
 import streamlit as st
 import sqlite3
 import pandas as pd
+from supabase import create_client
+
+pass_url = "https://www.streamlit.io"
+
+url="https://gywtlivqrvxgttetymwh.supabase.co"
+key="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd5d3RsaXZxcnZ4Z3R0ZXR5bXdoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQzMDQxMTAsImV4cCI6MjA1OTg4MDExMH0.1cL2AKAwyA_exgRE3qGh17KbYXa-gPDl93w-kjvAyAw"
+supabase=create_client(url,key)
 
 def creds_entered():
-     if st.session_state["user"].strip()=="admin" and st.session_state["password"].strip()=="admin123":
+     if st.session_state["user"].strip()=="admin@gmail.com" and st.session_state["password"].strip()=="admin123":
         st.session_state["authenticated"] = True
      else:
           st.session_state["authenticated"]=False
           if not st.session_state["user"]:
-              st.warning("Please enter your username.")
+              st.warning("Please enter your email.")
           elif not st.session_state["password"]:
               st.warning("Please enter your password.")
           else:
-             st.error("Invalid Username/Password") 
+             st.error("Invalid Email/Password. Please signup if you do not have an account") 
+             st.markdown("[Click here to signup](%s)" % pass_url)
 
 #https://www.youtube.com/watch?v=dOl51vhOGRc
 def authenticate_user():
@@ -20,15 +28,15 @@ def authenticate_user():
         placeholder=st.empty()
         with placeholder.container():
             st.title ("First, you need to login")
-            username=st.text_input("Enter your username: ", value="", max_chars=None, key="user", type="default", help=None, autocomplete=None, on_change=creds_entered, args=None, kwargs=None, placeholder="Type here", disabled=False, label_visibility="visible")
-            password=st.text_input("Enter your password: ", value="", max_chars=None, key="password", type="password", help=None, autocomplete=None, on_change=creds_entered, args=None, kwargs=None, placeholder="Type here", disabled=False, label_visibility="visible")
+            email=st.text_input("Enter your email: ", value="", max_chars=30, key="user", type="default", help=None, autocomplete=None, on_change=creds_entered, args=None, kwargs=None, placeholder="Type here", disabled=False, label_visibility="visible")
+            password=st.text_input("Enter your password: ", value="", max_chars=25, key="password", type="password", help=None, autocomplete=None, on_change=creds_entered, args=None, kwargs=None, placeholder="Type here", disabled=False, label_visibility="visible")
     else:
         if st.session_state["authenticated"]:
              return True
         else:
             st.title ("First, you need to login")
-            username=st.text_input("Enter your username: ", value="", max_chars=None, key="user", type="default", help=None, autocomplete=None, on_change=creds_entered, args=None, kwargs=None, placeholder="Type here", disabled=False, label_visibility="visible")
-            password=st.text_input("Enter your password: ", value="", max_chars=None, key="password", type="password", help=None, autocomplete=None, on_change=creds_entered, args=None, kwargs=None, placeholder="Type here", disabled=False, label_visibility="visible")
+            email=st.text_input("Enter your email: ", value="", max_chars=30, key="user", type="default", help=None, autocomplete=None, on_change=creds_entered, args=None, kwargs=None, placeholder="Type here", disabled=False, label_visibility="visible")
+            password=st.text_input("Enter your password: ", value="", max_chars=25, key="password", type="password", help=None, autocomplete=None, on_change=creds_entered, args=None, kwargs=None, placeholder="Type here", disabled=False, label_visibility="visible")
             return False 
 
 
@@ -44,7 +52,6 @@ def reports():
 
 
     with col3:
-        st.subheader("Model performance")
         conn = sqlite3.connect('fraud.db')  # New connection
         cursor = conn.cursor()  # Create cursor
         cursor.execute("SELECT * FROM xgb_classification_report")
@@ -56,13 +63,23 @@ def reports():
     displayTransaction()
 
 
+#def displayTransaction():
+    #conn = sqlite3.connect('fraud.db')  # New connection
+    #cursor = conn.cursor()  # Create cursor
+   # cursor.execute("SELECT * FROM transaction_z")
+   # results=cursor.fetchall()
+    #columns = [desc[0] for desc in cursor.description]
+   # df_transactions = pd.DataFrame(results, columns=columns)
+   # st.write("Click on the columns to sort them!")
+   # st.dataframe(df_transactions.head(10000))
+   # st.success("Transactions have been displayed on table")
+
 def displayTransaction():
-    conn = sqlite3.connect('fraud.db')  # New connection
-    cursor = conn.cursor()  # Create cursor
-    cursor.execute("SELECT * FROM transaction_z")
-    results=cursor.fetchall()
-    columns = [desc[0] for desc in cursor.description]
-    df_transactions = pd.DataFrame(results, columns=columns)
+    response = (
+        supabase.table("Transactions").select("*").execute()    
+    )
+    results=response.data
+    df_transactions=pd.DataFrame(results)
     st.write("Click on the columns to sort them!")
     st.dataframe(df_transactions.head(10000))
     st.success("Transactions have been displayed on table")
